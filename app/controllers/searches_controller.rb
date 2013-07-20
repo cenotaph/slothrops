12e40b466cd3b2428839dd1fb97509b2e9b93e9b
John W. Fail <fail@cenotaph.org>
2013-07-21 00:43:09 +0300

    temporarily removed ThinkingSphinx since we run on AppFog now and must convert to Searchify

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,17 +1,26 @@
 class SearchesController < ApplicationController
-  before_filter :get_feed  
+  before_filter :get_feed
+  
+  
   def create
-    hits = Book.search params[:searchterm]
-    hits += Edition.search params[:serchterm]
-    hits += ConsignmentItem.search params[:searchterm]
-    hits += Inventory.search params[:searchterm]
+    hits = Book.includes(:creator).where("title LIKE '%" + params[:searchterm].gsub(/[']/, '\\\\\'') + "%' OR creators.displayname LIKE '%#{params[:searchterm].gsub(/[']/, '\\\\\'')}%'")
+    hits += Edition.where("product_description LIKE '%" + params[:searchterm].gsub(/[']/, '\\\\\'') + "%'")
+    hits += ConsignmentItem.where("item_description LIKE '%" + params[:searchterm].gsub(/[']/, '\\\\\'') + "%'")
+    # hits = Book.search params[:searchterm]
+    # hits += Edition.search params[:serchterm]
+    # hits += ConsignmentItem.search params[:searchterm]
+    # hits += Inventory.search params[:searchterm]
     hits.compact!
     @hits = []
     hits.each do |hit|
       if hit.class == Book
         @hits.push(hit) unless hit.in_stock.empty?
       elsif hit.respond_to?(:book)
-        @hits.push(hit.book) unless hit.book.in_stock.empty?
+        if hit.book.nil?
+          @hits.push(hit) if hit.in_stock?
+        else
+          @hits.push(hit.book) unless hit.book.in_stock.empty?
+        end
       else
         @hits.push(hit)
       end
